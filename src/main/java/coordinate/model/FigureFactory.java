470ce525c3ea0ed2ef8820a5adb980aa8fbfccb7
nooose <hd15807@gmail.com>
2022-05-30 11:40:45 +0900

    refactor(FigureFactory): 함수형 리팩토링 - classifier 추가

--- src/main/java/coordinate/model/FigureFactory.java
+++ src/main/java/coordinate/model/FigureFactory.java
@@ -1,30 +1,41 @@
 package coordinate.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class FigureFactory {
     private static final String ERROR_INVALID_FIGURE_CREATION = "입력된 Point 개수가 유효하지 않습니다.";
     private static final int NUM_OF_VERTICES_OF_LINE = 2;
     private static final int NUM_OF_VERTICES_OF_TRIANGLE = 3;
     private static final int NUM_OF_VERTICES_OF_RECTANGLE = 4;
+    private static final Map<Integer, Function<List<Point>, Figure>> classifier = new HashMap<>();
+
+    static {
+        classifier.put(NUM_OF_VERTICES_OF_LINE, Line::new); // points -> new Line(points)
+        classifier.put(NUM_OF_VERTICES_OF_TRIANGLE, Triangle::new);
+        classifier.put(NUM_OF_VERTICES_OF_RECTANGLE, Rectangle::new);
+    }
 
     public static Figure create(List<Point> points) {
         if (points == null) {
             throw new IllegalArgumentException(AbstractFigure.ERROR_FIGURE_NULL);
         }
 
-        if (points.size() == NUM_OF_VERTICES_OF_LINE) {
-            return new Line(points);
+        if (isInvalidNumberOf(points)) {
+            throw new IllegalArgumentException(ERROR_INVALID_FIGURE_CREATION);
         }
 
-        if (points.size() == NUM_OF_VERTICES_OF_TRIANGLE) {
-            return new Triangle(points);
-        }
+        return classifyFigure(points);
+    }
 
-        if (points.size() == NUM_OF_VERTICES_OF_RECTANGLE) {
-            return new Rectangle(points);
-        }
+    private static boolean isInvalidNumberOf(List<Point> points) {
+        int numOfPoints = points.size();
+        return numOfPoints < NUM_OF_VERTICES_OF_LINE || numOfPoints > NUM_OF_VERTICES_OF_RECTANGLE;
+    }
 
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+    private static Figure classifyFigure(List<Point> points) {
+        return classifier.get(points.size()).apply(points);
     }
 }
